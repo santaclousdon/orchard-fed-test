2284b6907cc88a6c41d791df077f834bc2d3318f
santaclousdon <santaclous112@gmail.com>
2024-09-19 21:49:15 -0400

    add: favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,59 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        filename: 'js/main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass'),
-                        },
-                    },
-                ],
+  entry: "./src/js/main.js",
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
             },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]',
-                },
-            }
+          },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/main.css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-    ],
-    devtool: 'source-map',
-    mode: 'development',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
         },
-        compress: true,
-        port: 8080,
-        open: true,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/images/favicon.png",
+    }),
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
+    compress: true,
+    port: 8080,
+    open: true,
+  },
 };
